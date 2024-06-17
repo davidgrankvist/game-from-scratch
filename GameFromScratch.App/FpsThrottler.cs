030f671628dc64f4b6cc915b382f5a88ee0d7233
David Grankvist <grankvist.david@gmail.com>
2024-06-17 19:04:02 +0200

    move throttling to separate class

---
+++ GameFromScratch.App/FpsThrottler.cs
@@ -0,0 +1,39 @@
+﻿namespace GameFromScratch.App
+{
+	internal class FpsThrottler
+	{
+		// 1 tick = 100 ns = ten millionth of a second
+		private const int TICKS_PER_SECOND = 10_000_000;
+		private const int DEFAULT_FPS = 60;
+
+		private long lastTick;
+		private readonly double targetTicksPerFrame;
+
+		public FpsThrottler(int fps)
+		{
+			targetTicksPerFrame = GetTargetTicksPerFrame(fps);
+			lastTick = DateTime.MinValue.Ticks;
+		}
+
+        public FpsThrottler() : this(DEFAULT_FPS)
+        {
+        }
+
+        private static double GetTargetTicksPerFrame(int fps)
+		{
+			return TICKS_PER_SECOND / fps;
+		}
+
+		public bool PollIsReady()
+		{
+			var ellapsed = DateTime.UtcNow.Ticks - lastTick;
+			if (ellapsed < targetTicksPerFrame)
+			{
+				return false;
+			}
+
+			lastTick = DateTime.UtcNow.Ticks;
+			return true;
+		}
+	}
+}
--- GameFromScratch.App/Game.cs
+++ GameFromScratch.App/Game.cs
@@ -3,37 +3,32 @@
 	internal class Game
 	{
 		private Win32BitmapDrawer bitmapDrawer;
+		private FpsThrottler throttler;
 
-		private long lastTick = DateTime.MinValue.Ticks;
-		private const int TARGET_FPS = 60;
-		private const int TICKS_PER_SECOND = 10_000_000;
-		private const double TARGET_NANOSECONDS_PER_FRAME = TICKS_PER_SECOND / TARGET_FPS;
-
+		private long animationLastTick = DateTime.MinValue.Ticks;
 		private byte animationRed = 0;
 		private int animationSign = 1;
 
 		public Game(Win32BitmapDrawer bitmapDrawer)
 		{
 			this.bitmapDrawer = bitmapDrawer;
+			throttler = new FpsThrottler();
 		}
 
 		public bool RunFrame()
 		{
-			// throttle
-			var ellapsed = DateTime.UtcNow.Ticks - lastTick;
-			if (ellapsed < TARGET_NANOSECONDS_PER_FRAME)
+			if (!throttler.PollIsReady())
 			{
 				return false;
 			}
 
-			RunTestAnimation(ellapsed);
-
-			lastTick = DateTime.UtcNow.Ticks;
+			RunTestAnimation();
 			return true;
 		}
 
-		private void RunTestAnimation(long ellapsed)
+		private void RunTestAnimation()
 		{
+			var ellapsed = DateTime.UtcNow.Ticks - animationLastTick;
 			if (ellapsed > 1000)
 			{
 				if (animationRed == 120)
@@ -45,9 +40,10 @@
 					animationSign = 1;
 				}
 				animationRed = (byte)(animationRed + animationSign);
-			}
+				bitmapDrawer.Fill(animationRed, 0, 0);
 
-			bitmapDrawer.Fill(animationRed, 0, 0);
+				animationLastTick = DateTime.UtcNow.Ticks;
+			}
 		}
 	}
 }
