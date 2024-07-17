035408086ef389e01f9d38e7ecf8b58f12379e4c
David Grankvist <grankvist.david@gmail.com>
2024-07-17 21:44:24 +0200

    add gravity inverter device demo level

---
+++ GameFromScratch.App/Gameplay/Simulations/Levels/GravityInverterDeviceDemoLevel.cs
@@ -0,0 +1,38 @@
+﻿using GameFromScratch.App.Gameplay.Simulations.Entities;
+using System.Drawing;
+using System.Numerics;
+
+namespace GameFromScratch.App.Gameplay.Simulations.Levels
+{
+    internal class GravityInverterDeviceDemoLevel : ILevel
+    {
+        public IEnumerable<Entity> Create()
+        {
+            var mapSize = LevelUtils.MAP_SIZE;
+
+            var groundHeight = 50;
+            var groundBottomLeft = new Entity
+            {
+                Flags = EntityFlags.Solid | EntityFlags.Render,
+                Position = new Vector2(0, mapSize.Y - groundHeight),
+                Bounds = new Vector2(mapSize.X / 2, groundHeight),
+                Color = Color.Black,
+            };
+            var groundTopRight = new Entity
+            {
+                Flags = EntityFlags.Solid | EntityFlags.Render,
+                Position = new Vector2(mapSize.X / 2, 0),
+                Bounds = new Vector2(mapSize.X / 2, groundHeight),
+                Color = Color.Black,
+            };
+
+            var player = LevelUtils.CreatePlayer();
+
+            return [
+                groundBottomLeft,
+                groundTopRight,
+                player,
+            ];
+        }
+    }
+}
