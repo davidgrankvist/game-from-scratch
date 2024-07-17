3afc8be3f92dabd8cb6f3df4b3db21c052bc43c9
David Grankvist <grankvist.david@gmail.com>
2024-07-17 20:39:29 +0200

    make shrink device one-way

--- GameFromScratch.App/Gameplay/Simulations/Systems/ShrinkDeviceSystem.cs
+++ GameFromScratch.App/Gameplay/Simulations/Systems/ShrinkDeviceSystem.cs
@@ -6,7 +6,6 @@ namespace GameFromScratch.App.Gameplay.Simulations.Systems
     internal class ShrinkDeviceSystem : ISystem
     {
         private const float minScale = 0.5f;
-        private const float maxScale = 1;
         private float scale = 1;
         private const float scaleStep = 0.005f;
 
@@ -30,12 +29,6 @@ namespace GameFromScratch.App.Gameplay.Simulations.Systems
             if (input.IsDown(KeyCode.MouseLeft))
             {
                 scale = MathF.Max(scale - scaleStep, minScale);
-                didScale = true;
-            }
-
-            if (input.IsDown(KeyCode.MouseRight))
-            {
-                scale = MathF.Min(scale + scaleStep, maxScale);
                 didScale = true;
             }
 
